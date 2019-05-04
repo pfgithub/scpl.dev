8e9ada1c3ff2802b9d495a8b6c52639b90dd0489
pfg <omegariisthebest@gmail.com>
2019-05-04 16:18:27 -0700

    react-snap and scpl/shortcut-preview setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ShortcutPreview from "shortcut-preview";
+import {parse, inverse} from "scpl";
+
+class App extends Component<{}, {data: any, text: string}>{
+    constructor(props: Readonly<{}>){
+        super(props);
+        this.state = {data: undefined, text: `ShowResult "Hello ScPL"
+ChooseFromMenu items=["Getting Started", "View Documentation"]
+Case "Getting Started"
+    URL "https://docs.scpl.dev/gettingstarted"
+Case "View Documentation"
+    URL "https://docs.scpl.dev/"
+End Menu
+OpenURLs`};
+    }
+    componentWillMount(){
+        this.textareaChanged(this.state.text);
+    }
+    textareaChanged(value: string){
+        let data = parse(value, {make: ["shortcutjson"]});
+        this.setState({data: data.shortcutjson, text: value})
+    }
+    render(){
+        return <div><textarea onKeyUp={(e) => this.textareaChanged(e.currentTarget.value)}>{this.state.text}</textarea><ShortcutPreview data={this.state.data}></ShortcutPreview></div>
+    }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+if(!rootElement) {throw new Error("root element does not exist");}
+if(rootElement.hasChildNodes()){
+	ReactDOM.hydrate(<App />, document.getElementById('root'));
+}else{
+	ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
